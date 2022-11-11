8ae371ec81fc197bf5ed208c100e327bf0b0882c
LuizFelpsc <117685654+LuizFelpsc@users.noreply.github.com>
2022-11-11 19:34:45 -0300

    Add files via upload

---
+++ Tabuada.py
@@ -0,0 +1,15 @@
+#Tabuada
+
+n = int(input("Digite Um Numero Para Ver a Tabuada : "))
+print("-"*15)
+print("  {} X 1 = {}".format(n,n*1))
+print("  {} X 2 = {}".format(n,n*2))
+print("  {} X 3 = {}".format(n,n*3))
+print("  {} X 4 = {}".format(n,n*4))
+print("  {} X 5 = {}".format(n,n*5))
+print("  {} X 6 = {}".format(n,n*6))
+print("  {} X 7 = {}".format(n,n*7))
+print("  {} X 8 = {}".format(n,n*8))
+print("  {} X 9 = {}".format(n,n*9))
+print("  {} X 10 = {}".format(n,n*10))
+print("-"*15)
